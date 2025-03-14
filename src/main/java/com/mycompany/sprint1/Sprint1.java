099bad9ce912aff229f3a995c6f19d5cf14bbcb9
TranYen05 <tranthihaiyenqb05@gmail.com>
2025-03-14 08:04:18 +0700

    First commit

---
+++ src/main/java/com/mycompany/sprint1/Sprint1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sprint1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Sprint1 {
+
+    public static void main(String[] args) {
+         java.awt.EventQueue.invokeLater(() -> new dienTichHinhTron().setVisible(true));
+    }
+}
